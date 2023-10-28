cd36df21a63c46eaa37b472130f82f9e95e5c6f8
Welkus <vdelkus@gmail.com>
2023-10-29 02:10:30 +0300

    Task done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
 
 public class FileReader {
 
+
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        String name;
+        int age;
+        String email;
+        long phone;
+
+        try (java.io.FileReader fileReader = new java.io.FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            name = bufferedReader.readLine().split(": ")[1];
+            age = Integer.parseInt(bufferedReader.readLine().split(": ")[1].trim());
+            email = bufferedReader.readLine().split(": ")[1];
+            phone = Long.parseLong(bufferedReader.readLine().split(": ")[1].trim());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Profile(name, age, email, phone);
     }
 }
+
